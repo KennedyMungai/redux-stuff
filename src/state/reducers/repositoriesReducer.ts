4797191ac9fd2e8018ce57d0c0181a6547022b84
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-22 13:01:10 +0300

    The basic setup code

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,20 +1,11 @@
 interface RepositoriesState {
   loading: boolean;
-  error: string | null;
+  error: string;
   data: string[];
 }
 
-const reducer = (state: RepositoriesState, action: any): RepositoriesState => {
-  switch (action.type) {
-    case 'search_repositories':
-      return { loading: true, error: null, data: [] };
-    case 'search_repositories_success':
-      return { loading: false, error: null, data: action.payload };
-    case 'search_repositories_error':
-      return { loading: false, error: action.payload, data: [] };
-    default:
-      return state;
-  }
-};
+const reducer = (state, action) => {
+
+}
 
 export default reducer;
