8da158daf357333fc76c7b4c399c3c79781d6b40
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-22 15:45:03 +0300

    Move dthe enum actiontype to the index ts file

---
+++ src/state/action-types/index.ts
@@ -0,0 +1,5 @@
+enum ActionType {
+    SEARCH_REPOSITORIES = 'search_repositories',
+    SEARCH_REPOSITORIES_ERROR = 'search_repositories_error',
+    SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success'
+};
