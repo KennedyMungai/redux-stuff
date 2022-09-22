d951189261164126cb505ced22b30caaa043d898
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-22 13:12:58 +0300

    Replaced null with an empty string

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -7,9 +7,9 @@ interface RepositoriesState {
 const reducer = (state: RepositoriesState, action: any): RepositoriesState => {
   switch (action.type) {
     case 'search_repositories':
-      return { loading: true, error: null,data:[]  }
+      return { loading: true, error: '',data:[]  }
     case 'search_repositories_success':
-      return { loading: false, error: null, data: action.payload }
+      return { loading: false, error: '', data: action.payload }
     case 'search_repositories_error':
       return { loading: false, error: action.payload, data:[] }
     default:
