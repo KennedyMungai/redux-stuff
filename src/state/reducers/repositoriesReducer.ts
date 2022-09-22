ab61bff23a5a2423f46d42877e85a6a336046ef0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-22 15:45:22 +0300

    Moved the enum

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -4,11 +4,7 @@ interface RepositoriesState {
   data: string[];
 }
 
-enum ActionType {
-  SEARCH_REPOSITORIES = 'search_repositories',
-  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error',
-  SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success'
-};
+
 
 const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
   switch (action.type) {
