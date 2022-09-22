a93b53baa51544994043cc7460e8dbfb683c2023
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-22 15:14:55 +0300

    File was formatted

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -21,11 +21,11 @@ interface SearchRepositoriesErrorAction {
 const reducer = (state: RepositoriesState, action: SearchRepositoriesAction | SearchRepositoriesErrorAction | SearchRepositoriesSuccessAction): RepositoriesState => {
   switch (action.type) {
     case 'search_repositories':
-      return { loading: true, error: '',data:[]  }
+      return { loading: true, error: '', data: [] }
     case 'search_repositories_success':
       return { loading: false, error: '', data: action.payload }
     case 'search_repositories_error':
-      return { loading: false, error: action.payload, data:[] }
+      return { loading: false, error: action.payload, data: [] }
     default:
       return state;
   }
