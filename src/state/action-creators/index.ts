fd905df35c2180500aee3a992f2093e1c7396491
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-22 15:58:33 +0300

    Created a function called SearchRepositories which takes in a search term and returns an async function

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
+
+const searchRepositories = (term: string) => {
+    return async (dispatch: any) => {
+        
+    };
+};
