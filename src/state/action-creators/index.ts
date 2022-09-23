da5839ead7fe4ef72dea32bed8890497943c689d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-23 09:56:58 +0300

    Added an import for Dispatch from redux

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { ActionType } from "../action-types";
 import { Action } from "../actions";
+import { Dispatch } from "redux";
 
 
 export const searchRepositories = (term: string) => {
