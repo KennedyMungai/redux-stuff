93edc31a37667ea1b01f37b13759af4860360753
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-23 11:39:05 +0300

    Data is printed out when there is no error or loading

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -22,6 +22,7 @@ const RepositoriesList: React.FC = () => {
             </form>
             {error && <h3>{error}</h3>}
             {loading && <h3>Loading...</h3>}
+            {!error && !loading && data}
         </div>
     )
 }
