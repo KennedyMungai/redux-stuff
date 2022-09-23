a245ae6517b16193d1bcdc393a663c8ccea2d66b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-23 09:54:43 +0300

    Made the search repositories function an export function

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -3,7 +3,7 @@ import { ActionType } from "../action-types";
 import { Action } from "../actions";
 
 
-const searchRepositories = (term: string) => {
+export const searchRepositories = (term: string) => {
     return async (dispatch: any) => {
         dispatch({
             type: ActionType.SEARCH_REPOSITORIES
